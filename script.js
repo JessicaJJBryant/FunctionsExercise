18e081c7f5854567c8697de18a03f3ec8e3bddc8
Jessica Bryant <jessica.bryant@live.com>
2021-10-20 00:21:55 -0400

    Submit the FunctionsExercise

---
+++ script.js
@@ -0,0 +1,60 @@
+// FUNCTIONS EXERCISE
+
+// 1.
+function spookyGhost() {
+    console.log("Boo!!!");
+}
+spookyGhost();
+
+// 2.
+function candy(goal) {
+    console.log(`${goal}! ${goal}!`);
+    console.log(`${goal}! ${goal}!`);
+}
+candy(`Get Candy`);
+
+// 3.
+function werewolf(a, b) {
+    if (a === "Full" && b === "Moon") {
+        console.log(`ARH-WOOO!`);
+    }  
+}
+werewolf("Full", "Moon");
+
+// 4.
+function halloween() {
+    return "October 31st";
+}
+halloween();
+
+// 5.
+function isEnoughCandy(pieces) {
+    if (pieces >= 31) {
+        return true;
+    }
+    return false;
+}
+let enough = isEnoughCandy(200);
+console.log(enough);
+
+// 6.
+
+
+
+// 7.
+const hauntedMansion = function() {
+    console.log("Welcome, foolish mortals, to the Haunted Mansion!");
+}
+hauntedMansion();
+
+// 8.
+ghostbusters = () => "Who You Gonna Call?";
+
+
+// 9. no
+
+// 10. yes
+
+// 11a. 
+const costumes = ["Ghost", "Princess", "Pirate"];
+// 11b.
